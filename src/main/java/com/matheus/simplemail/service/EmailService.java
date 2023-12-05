80b30006df10bf45a7e30d1dfcd8537976597496
Mathe <matheusamrl@gmail.com>
2023-12-05 20:09:49 -0300

    Service e model feitos

---
+++ src/main/java/com/matheus/simplemail/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.matheus.simplemail.service;
+
+import com.matheus.simplemail.model.Email;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(Email email) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom(email.getFrom());
+        mimeMessageHelper.setTo(email.getTo());
+        mimeMessageHelper.setSubject(email.getSubject());
+        mimeMessageHelper.setText(email.getText());
+
+
+        javaMailSender.send(mimeMessage);
+
+    }
+}
---
+++ src/test/java/com/matheus/simplemail/SimplemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.simplemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
